3aafe57dbe4635c793b9904293f10204f781fbb4
Austen Higgins-Cassidy <plasmarobo@gmail.com>
2015-07-23 18:05:14 -0400

    Profiles!

---
+++ src/profiles/large.js
@@ -0,0 +1,12 @@
+var large_symbol = {
+  height: 200,
+  width: 40,
+  radius: 15,    //dot radius
+  thickness: 8, //dash thickness
+  padding: 8,
+  box_color: 'black',
+  feature_color: 'white',
+  unknown_color: 'grey'
+}
+
+registerProfile('large', large_symbol);
